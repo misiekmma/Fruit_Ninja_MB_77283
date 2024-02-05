7b5927294aab5b4dea29f8269517b5762e4bb4aa
Michał <Micha@LAPTOP-D9CTVM6T>
2024-02-05 17:34:36 +0100

    Dodaj pliki projektów.

---
+++ Fruit_Ninja/resource.h
@@ -0,0 +1,34 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Fruit_Ninja.rc
+//
+#define IDC_MYICON                      2
+#define IDD_FRUITNINJA_DIALOG           102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_FRUITNINJA                  107
+#define IDI_SMALL                       108
+#define IDC_FRUITNINJA                  109
+#define IDR_MAINFRAME                   128
+#define IDI_FRUIT                       130
+#define IDC_KNIFE                       131
+#define ID_FILE_EXIT                    32771
+#define ID_BOARD_MEDIUM                 32772
+#define ID_BOARD_BIG                    32773
+#define ID_BOARD_SMALL                  32774
+#define ID_FILE_NEW                     32775
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
